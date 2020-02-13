03f23ed9bd5a252fed4a0394ab1ea4c18e8fcf9e
nateguse <59934005+nateguse@users.noreply.github.com>
2020-02-13 12:01:18 -0500

    mgmt call

--- WhaleWars/Program.cs
+++ WhaleWars/Program.cs
@@ -6,31 +6,33 @@ namespace WhaleWars
     {
         public static void Main(string[] args)
         {
-           /* Whale barney = new Whale("barney", CharClass.fighter, 10, 2, 2);
+            /* Whale barney = new Whale("barney", CharClass.fighter, 10, 2, 2);
 
-            Planet sumPlanet = new Planet("Blowholia Prime", PlanetType.ocean, 5,4,Sector.Blowholia);
+             Planet sumPlanet = new Planet("Blowholia Prime", PlanetType.ocean, 5,4,Sector.Blowholia);
 
-            string Location = sumPlanet.Name;
-            int Turn = 0;
-            Console.Title = "Whale Wars";
+             string Location = sumPlanet.Name;
+             int Turn = 0;
+             Console.Title = "Whale Wars";
 
 
-            while (Turn < 20)
-            {
-                Turn++;
-                HUD(barney.Name, Location, Turn, barney.Health, barney.Offense, barney.Defense);
-                Console.WriteLine("Press enter to simulate turns progressing... turn number " + Turn + " ... ");
-                Console.ReadKey();
-                
-            }
+             while (Turn < 20)
+             {
+                 Turn++;
+                 HUD(barney.Name, Location, Turn, barney.Health, barney.Offense, barney.Defense);
+                 Console.WriteLine("Press enter to simulate turns progressing... turn number " + Turn + " ... ");
+                 Console.ReadKey();
 
-            Console.WriteLine("Simulation of game ending");
-            Console.ReadKey(); */
+             }
 
-            Weapon charwp = Weapon.CreateWeapon(WeaponList.Sword);
-            Console.WriteLine(charwp.Name);
-            Console.WriteLine(charwp.Damage);
-            Console.WriteLine(charwp.Defense);
+             Console.WriteLine("Simulation of game ending");
+             Console.ReadKey(); */
+
+            //Weapon charwp = Weapon.CreateWeapon(WeaponList.Sword);
+            //Console.WriteLine(charwp.Name);
+            //Console.WriteLine(charwp.Damage);
+            //Console.WriteLine(charwp.Defense);
+
+            Management.mgmt();
         }
     }
 }
