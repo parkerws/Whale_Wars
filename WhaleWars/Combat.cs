e389b1e3d71427b15b4778b6cfda02233c324e87
chaze-stoner <chaze.stoner@gmail.com>
2020-02-13 12:26:35 -0500

    Removed the HUD method from Combat as it is in its own class now.

--- WhaleWars/Combat.cs
+++ WhaleWars/Combat.cs
@@ -76,55 +76,7 @@ namespace WhaleWars
             user.Defense += 20;
             return user.Defense;
         } //Increases Defense
-        public static void HUD(string player, string location, int Health, int Attack, int Defence)
-        {
-
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write("*************************************");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Write("Whale-Wars");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("*************************************");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("====================================================================================");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Write("    " + player);
-            Console.ResetColor();
-            Console.Write(" :: ");
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write(location);
-            Console.ResetColor();
-            Console.Write(" :: ");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write(Health + " Health ");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write(Attack + " Attack ");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write(Defence + " Defence \n");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("====================================================================================\n\n\n\n");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine(" The Combat and Story Text will Apear hear! ");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(" When you take Damage it will apear in RED! ");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(" When you Attack it will apear in YELLOW! ");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(" When you Heal it will apear in GREEN!\n");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("====================================================================================");
-
-        } //Moved the Hud into the combat area, as its not needed in transition.
+       
         public static int EnemyAI(Whale user, Whale target)
         {
             Random r = new Random();
