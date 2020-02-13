3eb814e7842f56e07de59fe2cee434ea5f55ebb1
chaze-stoner <chaze.stoner@gmail.com>
2020-02-13 12:40:35 -0500

    Created a Mage Class to store all the methods for the mage. IE, the combat choices, as well as the method used to make the mage fight an enemy.  The idea is to later include a if statement based off of class that will generate what combat moves you get. (allows the choice of class to feel important.)

---
+++ WhaleWars/Mage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhaleWars
+{
+    class Mage
+    {
+        //Methods used to represent a Mage spell as damage to a target.
+        //######################################################################################################
+        public static int MageMoves(Whale user, Whale target)
+        {
+            Console.WriteLine("Select a skill to use\n" +
+                "1) Magic Missle\n" +
+                "2) Fire Ball\n" +
+                "3) Blood Syphon\n" +
+                "4) ArcaneShield");
+
+            int Input = Convert.ToInt32(Console.ReadLine());
+
+            switch (Input)
+            {
+                case int n when n == 1: return MagicMissle(user, target);
+                case int n when n == 2: return FireBall(user, target);
+                case int n when n == 3: return BloodSyphon(user, target);
+                case int n when n == 4: return ArcaneShield(user);
+            }
+
+            return Wand(user, target);
+        } //Allows the user to pick a skill           //#
+        public static int Wand(Whale user, Whale target)
+        {
+            target.Health -= (user.Offense - target.Defense);
+            return target.Health;
+        } //Basic attack                                   //#
+        public static int MagicMissle(Whale user, Whale target)
+        {
+            target.Health -= user.Offense;
+            return target.Health;
+        } //Deals more damage than basic attack     //#
+        public static int FireBall(Whale user, Whale target)
+        {
+            target.Health -= (user.Offense + 2);
+            return target.Health;
+        } //Deals more damage than Magic Missle        //#
+        public static int BloodSyphon(Whale user, Whale target)
+        {
+            target.Health -= user.Offense;
+            user.Health += user.Offense;
+            return target.Health;
+        } //Deals damage and absorbes health        //#
+        public static int ArcaneShield(Whale user)
+        {
+            Console.WriteLine("You are surrounded with a purple barrier.");
+            user.Defense += 20;
+            return user.Defense;
+        } //Increases Defense                                    //#
+        //######################################################################################################
+    }
+}
