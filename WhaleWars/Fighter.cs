beaae1bb1b5049ec2c7147683032f621f7ddd148
chaze-stoner <chaze-stoner@users.noreply.github.com>
2020-02-13 22:01:19 -0500

    Created the Fighter and Ranger Class so i can work out the combat menus.

---
+++ WhaleWars/Fighter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhaleWars
+{
+    class Fighter
+    {
+        public static int Lung(Whale user, Whale target)
+        {
+            target.Health -= (user.Offense - target.Defense);
+            return target.Health;
+        }
+        public static int Execute(Whale user, Whale target)
+        {
+            double percent = (target.Health / 10);
+            if (percent == 0.4) { target.Health -= target.Health; return target.Health; }
+            else { target.Health -= (user.Offense + 1); return target.Health;  }
+        }
+    }
+}
