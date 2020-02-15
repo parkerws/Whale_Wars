3812b03b8279a4b9fae93c3031a4ecc0b943d749
chaze-stoner <chaze-stoner@users.noreply.github.com>
2020-02-15 15:35:51 -0500

    Revert "Fixed a bug in the weapon select."
    This reverts commit fc135afda6958089cc00bb846b6921168a9e94bc.

--- WhaleWars/Program.cs
+++ WhaleWars/Program.cs
@@ -7,8 +7,9 @@ namespace WhaleWars
         public static void Main()
         {
             
-
-            Management.mgmt();
+            Whale dave = new Whale("steve", CharClass.fighter,10,10,10,ConsoleInterface.ShopList());
+            ConsoleInterface.DisplayInventory(dave, ConsoleInterface.ShopList());
+            //Management.mgmt();
 
 
         }
