bbfd4b1fdba6d312924b331a24516c0dd6f0f3c4
nateguse <59934005+nateguse@users.noreply.github.com>
2020-02-18 21:53:01 -0500

    Game Class

---
+++ WhaleWars/Game.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhaleWars
+{
+    class Game
+    {
+        
+        //Character name
+        static string characterName = "";
+        //Print out game title and setup
+
+        public static void startGame()
+        {
+            Fastwrite("Whale Wars!", "blue\n");
+            Console.WriteLine("\nPress Space to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            Typewrite("\nWelcome to the console based adventure game!", "dialog\n\n");
+            Fastwrite("\nYou wake up floating through space...", "dialog\n\n");
+            Console.WriteLine("\nPress space to continue...");
+            Console.ReadKey();
+            nameCharacter();
+
+        }
+        //ask plaer for a name and save it
+        static void nameCharacter()
+        {
+            Console.Clear();
+            Typewrite("\nWhats my name?", "self");
+            Fastwrite("\nType in your name: ", "dialog\n");
+            characterName = Console.ReadLine();
+            Typewrite($"\n{characterName}", "yellow");
+            Typewrite("...ya, It's all coming back to me now", "self");
+            Typewrite("\n... What happened?", "self");
+            Typewrite("\n... Where am I?", "self");
+            Console.WriteLine("\nPress Space to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            Fastwrite("\nYou look around... You see thousands of stars...", "dialog");
+            Typewrite("\n...I must have blacked out...", "self");
+            Typewrite("\n...I don't remember anything.", "self");
+            Console.WriteLine("\nPress Space to continue.");
+            Console.ReadKey();
+            Console.Clear();
+            Fastwrite("\nYou see a small planet off in the distance...", "dialog");
+            Typewrite("\nThat looks like Blowholia Prime... wait...", "self");
+            Typewrite("\nBlowholia... I think I was there... something bad happened...", "self");
+            Typewrite("\nI can't remember... maybe there are some answers there", "self");
+            Console.WriteLine("\nPress Space to continue.");
+            Console.ReadKey();
+            Console.Clear();
+            Choice();
+        }
+        static void Choice()
+        {
+            string input = "";
+            Typewrite($"\n{characterName}", "yellow");
+            Fastwrite(", Do you want to go too Blowholia Prime?", "dialog");
+            Console.WriteLine("\nA) Yes" +
+                "\nB) No");
+            input = input.ToUpper();
+            input = Console.ReadLine();
+            if (input == "A")
+            {
+                Fastwrite("\n You start to swim through the cold empty space, your eyes set on Blowholia Prime", "dialog");
+            }
+            else
+            {
+                Typewrite("\n I need to find out what happened to me", "self");
+            }
+
+        }
+        static void Typewrite(string message, string color)
+        {
+            if (color == "enemy")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (color == "friend")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (color == "yellow")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (color == "self")
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            }
+            else if (color == "dialog")
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (color == "blue")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                System.Threading.Thread.Sleep(45);
+
+            }
+            Console.ResetColor();
+
+        }
+        static void Fastwrite(string message, string color)
+        {
+            if (color == "enemy")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (color == "friend")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (color == "yellow")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (color == "self")
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            }
+            else if (color == "dialog")
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (color == "blue")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                System.Threading.Thread.Sleep(25);
+
+            }
+            Console.ResetColor();
+
+        }
+    }
+
+}
