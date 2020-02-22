b95049bd6513a4b53acc3a4099d1727734ce3198
nateguse <59934005+nateguse@users.noreply.github.com>
2020-02-22 13:45:39 -0500

    fds

--- WhaleWars/Game.cs
+++ WhaleWars/Game.cs
@@ -659,7 +659,7 @@ namespace WhaleWars
             for (int i = 0; i < message.Length; i++)
             {
                 Console.Write(message[i]);
-                System.Threading.Thread.Sleep(35);
+                System.Threading.Thread.Sleep(30);
 
             }
             Console.ResetColor();
