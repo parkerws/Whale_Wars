95b61d1419100f9430cbcd190051e56ae6437fac
chaze-stoner <chaze-stoner@users.noreply.github.com>
2020-02-22 12:23:47 -0500

    forgot to add the ShipIntro into game, here it is.

--- WhaleWars/Game.cs
+++ WhaleWars/Game.cs
@@ -101,7 +101,7 @@ namespace WhaleWars
             }
             else
             {
-                ConsoleInterface.Ship(Player);
+                ConsoleInterface.ShipIntro(Player);
             }
 
         }
