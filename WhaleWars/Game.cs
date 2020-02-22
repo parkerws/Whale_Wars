af5c65a611b76000dd7832ec9ee9b922109bd1cd
nateguse <59934005+nateguse@users.noreply.github.com>
2020-02-22 13:37:22 -0500

    text speed up

--- WhaleWars/Game.cs
+++ WhaleWars/Game.cs
@@ -659,7 +659,7 @@ namespace WhaleWars
             for (int i = 0; i < message.Length; i++)
             {
                 Console.Write(message[i]);
-                System.Threading.Thread.Sleep(45);
+                System.Threading.Thread.Sleep(35);
 
             }
             Console.ResetColor();
